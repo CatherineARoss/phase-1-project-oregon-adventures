98f6c8519a4afad25997324bb94a9f51478623ff
Catherine Ross <fibervibes@gmail.com>
2023-05-02 11:09:44 -0400

    adds image to header

--- main.js
+++ main.js
@@ -1,6 +1,10 @@
-fetch("http://localhost:3000/Adventures")
-    .then(response => response.json())
-    .then((adventures) => {
-        const container = document.querySelector("ul#adventureList");
-        adventures.forEach(adventure)
-    });
+   // fetch("http://localhost:3000/Adventures")
+    //    .then(response => response.json())
+     //   .then((adventures) => {
+     //       const container = document.querySelector("ul#adventureList");
+     //       adventures.forEach(adventure => {
+    //        const li = document.createElement("li");
+    //        li.textContent = adventure.title;
+    //        container.appendChild(li);
+//        });
+//    });
