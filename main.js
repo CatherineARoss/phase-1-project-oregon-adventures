14c36e67f731d7d6ee94c291da76aad42c6136a4
Catherine Ross <fibervibes@gmail.com>
2023-05-06 08:29:18 -0400

    alligns JSON content within image border

--- main.js
+++ main.js
@@ -1,12 +1,23 @@
-
+const adventureWrapper = document.querySelectorAll('.adventure-image-wrapper');
 
 document.addEventListener('DOMContentLoaded', () => {
 fetch('http://localhost:3000/adventures')
-.then((response) => response.json())
-.then((data) => {
-  console.log(data);
-})
-.catch((error) => {
-  console.error('Error fetching data:', error);
-})
-});
+  .then((response) => response.json())
+  .then((adventures) => {
+      adventures.forEach((adventure) => {
+        const adventureWrapper = document.getElementById(`adventure-${adventure.id}`);
+
+        const adventureName = document.createElement('h3');
+        adventureName.textContent = `${adventure.name}`;
+        adventureWrapper.appendChild(adventureName);
+
+        const adventureDescription = document.createElement('p');
+        adventureDescription.textContent = `${adventure.description}`;
+        adventureWrapper.appendChild(adventureDescription);
+
+        const adventureGear = document.createElement('p');
+        adventureGear.textContent = `${adventure.gear}`;
+        adventureWrapper.appendChild(adventureGear);
+      });
+    })
+  });
