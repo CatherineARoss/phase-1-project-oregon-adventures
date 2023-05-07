9282ed0bb12fde9e01406bf2679ae5c41488807f
Catherine Ross <fibervibes@gmail.com>
2023-05-07 13:45:13 -0400

    formats code to read easier

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 const adventureWrapper = document.querySelectorAll('.adventure-image-wrapper');
 
 document.addEventListener('DOMContentLoaded', () => {
-fetch('http://localhost:3000/adventures')
-  .then((response) => response.json())
-  .then((adventures) => {
+  fetch('http://localhost:3000/adventures')
+    .then((response) => response.json())
+    .then((adventures) => {
       adventures.forEach((adventure) => {
         const adventureWrapper = document.getElementById(`adventure-${adventure.id}`);
 
@@ -20,53 +20,53 @@ fetch('http://localhost:3000/adventures')
         adventureWrapper.appendChild(adventureGear);
       });
     })
-  });
+});
 
 
-  const bookNowButtons = document.querySelectorAll('.book-now');
+const bookNowButtons = document.querySelectorAll('.book-now');
 
-  document.addEventListener('click', function(e) {
-    if (e.target.classList.contains('book-now')) {
-      e.preventDefault();
-      const participantsInput = e.target.parentElement.querySelector('.participants');
-      const numberOfParticipants = participantsInput.value;
-      if (numberOfParticipants > 0 && numberOfParticipants <= 10) {
-        alert(`Booking for ${numberOfParticipants} participants, a confirmation Email will be sent to you.`);
-      } else if (numberOfParticipants > 10) {
-        alert(`Fully Booked: Max is 10 participants`);
-      }
+document.addEventListener('click', function (e) {
+  if (e.target.classList.contains('book-now')) {
+    e.preventDefault();
+    const participantsInput = e.target.parentElement.querySelector('.participants');
+    const numberOfParticipants = participantsInput.value;
+    if (numberOfParticipants > 0 && numberOfParticipants <= 10) {
+      alert(`Booking for ${numberOfParticipants} participants, a confirmation Email will be sent to you.`);
+    } else if (numberOfParticipants > 10) {
+      alert(`Fully Booked: Max is 10 participants`);
     }
-  });
+  }
+});
 
 
 const emailElement = document.querySelector('#email');
 const messageElement = document.querySelector('#message');
 const submitButton = document.querySelector('#submit-button');
-submitButton.addEventListener('click', function(e) {
-	e.preventDefault();
+submitButton.addEventListener('click', function (e) {
+  e.preventDefault();
 
-	const emailValue = emailElement.value;
-	const messageValue = messageElement.value;
+  const emailValue = emailElement.value;
+  const messageValue = messageElement.value;
 
-	console.log('Email: ', emailValue);
-	console.log('Message: ', messageValue);
+  console.log('Email: ', emailValue);
+  console.log('Message: ', messageValue);
 
-	if(emailValue.includes('@')) {
-		alert('Thank You');
-	} else {
-		alert('Please enter a valid email address.');
-	}
+  if (emailValue.includes('@')) {
+    alert('Thank You');
+  } else {
+    alert('Please enter a valid email address.');
+  }
 });
 
 
 const gallery = document.querySelector('.gallery');
 const image = gallery.querySelector('.gallery-image');
 
-image.addEventListener('mouseover', function() {
+image.addEventListener('mouseover', function () {
   image.style.opacity = '0.5';
 });
 
-image.addEventListener('mouseout', function() {
+image.addEventListener('mouseout', function () {
   image.style.opacity = '1';
 });
 
